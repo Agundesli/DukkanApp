6a9b2ed8e36174f1c5a375f8a25df25f3d6f8231
Agundesli <agundesli06@gmail.com>
2023-07-18 03:01:07 +0300

    LogOut

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,48 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, FlatList, View } from 'react-native';
+import { SafeAreaView, FlatList, Button, View } from 'react-native';
 import Config from 'react-native-config';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import useFetch from '../../hooks/useFetch';
 import Loading from '../../components/Loading/Loading';
 import Error from '../../components/Error/Error';
-import Button from '../../components/Button/Button';
+import {useDispatch} from 'react-redux';
 
 const Products = ({ navigation }: any) => {
 
-
-    // const[error, setError]=useState(String);
-    // const[loading, setLoading]=useState(true);
-    // const[data,setData]=useState([]);
-
-    // useEffect(() =>{
-    //     fetchData();
-    // }, []);
-
-    // const fetchData = async() =>{
-    //     try {
-    //         const {data:productData} = await axios.get(Config.API_URL || '');
-    //         //as String:Bu, hatayı ortadan kaldıracaktır, ancak herhangi bir şekilde bu bir dizi değilse, bir çalışma zamanı hatası alırsınız... 
-    //         //türün eşleşmesini sağlamak ve bu tür hatalardan kaçınmak için TypeScript kullanmamızın nedenlerinden biri de budur.
-    //         //Derleme hatasını önlemek için, Bu, hatayı ortadan kaldırabilir, ancak değişkenin gerçek türünü asla değiştirmez.
-    //         //Ya bu bir dizi değil de nesneyse? Eğer bir nesne ise '' yerine {} olacaktır.
-    //         // const {data} = await axios.get(Config.API_URL ?? '');
-    //         // const {data} = await axios.get(Config.API_URL!);
-    //         // const {data} = await axios.get(Config.API_URL as string);
-    //         setData(productData);
-    //         setLoading(false);    
-    //     } catch (ex) {
-    //         let errorMessage = "Failed to do something exceptional";
-    //         if (ex instanceof Error) {
-    //             errorMessage = ex.message;
-    //           }
-    //         setError(errorMessage);
-    //         setLoading(false);
-    //     }
-    //     // catch(err:any){
-    //     //     setError(err.messgae)
-    //     // }
-    // };
-
+    const dispatch =useDispatch();
     const { loading, data, error } = useFetch(Config.API_PRODUCT_URL);
 
     const handleProductSelect = (id) => {
@@ -65,12 +32,15 @@ const Products = ({ navigation }: any) => {
 
     return (
         <SafeAreaView>
+            <View>
+                <Button title='logout' onPress={()=>dispatch({type:'SET_USER', payload:{user:null}})}/>
+                <FlatList data={data} renderItem={renderProduct} />
+                {/* <FlatList  data={[1,2,3]} renderItem={data=><Text>{data.item}</Text>}  /> */}
+                {/* Data propertisi ben ekrana ne basmak istiyorsam onun Array halidir */}
+                {/* RenderItem propertisi biz ekrana componenti nasıl bamak istiyorsak, bize o noktada yardımcı olan bir callback function */}
+                {/* Devcut data sayısı kadar RenderItem tetiklenir ve ekrana verileri basar */}
+            </View>
 
-            <FlatList data={data} renderItem={renderProduct} />
-            {/* <FlatList  data={[1,2,3]} renderItem={data=><Text>{data.item}</Text>}  /> */}
-            {/* Data propertisi ben ekrana ne basmak istiyorsam onun Array halidir */}
-            {/* RenderItem propertisi biz ekrana componenti nasıl bamak istiyorsak, bize o noktada yardımcı olan bir callback function */}
-            {/* Devcut data sayısı kadar RenderItem tetiklenir ve ekrana verileri basar */}
 
         </SafeAreaView>
 
